8c6a4307d8723b023b271fe0e1c64c9e01555c14
MasterZeeno <masterzeeno@outlook.com>
2025-07-26 16:29:14 +0800

    Updated!

--- SSR.py
+++ SSR.py
@@ -73,15 +73,15 @@ for i, row in enumerate(cleaned_data):
 
 # Full HTML document
 html_content = f"""<div style="background:0 0;margin:0;padding:0;border:0 none transparent;outline:0 none transparent;width:100%;height:auto;box-sizing:border-box">
-  <table width="100%" cellspacing="0" cellpadding="0" style="font-size:16px !important;max-width:537px !important;min-width:280px !important;width:96.69% !important">
+  <table width="100%" cellspacing="0" cellpadding="0" style="font-size:16px !important;max-width:537px !important;min-width:280px !important;width:96.69% !important;box-sizing:border-box">
     <tbody>
       <tr>
         <td align="left">
-          <table width="100%" cellspacing="0" cellpadding="0" border="0" style="font-family:Helvetica,system-ui,sans-serif;font-size:clamp(.5em,2.353vw + .059em,1em)!important;line-height:clamp(.4em,6.588vw + -.835em,1.8em)!important;padding:0 clamp(1.125em,3.882vw + .397em,1.95em) 0!important;color:#0061ba!important;text-align:left;word-spacing:normal;letter-spacing:normal">
+          <table width="100%" cellspacing="0" cellpadding="0" border="0" style="font-family:Helvetica,system-ui,sans-serif;font-size:clamp(.5em,2.353vw + .059em,1em)!important;line-height:clamp(.4em,6.588vw + -.835em,1.8em)!important;padding:0 clamp(1.125em,3.882vw + .397em,1.95em) 0!important;color:#0061ba!important;text-align:left;word-spacing:normal;letter-spacing:normal;box-sizing:border-box">
             <tbody>
               <tr>
                 <td>
-                  <div style="margin:clamp(1.125em,3.882vw + .397em,1.95em) auto 0;font-size:0.96em;">
+                  <div style="margin:clamp(1.125em,3.882vw + .397em,1.95em) auto 0;font-size:0.96em;box-sizing:border-box">
                     <h3>Good day, everyone!</h3>
                     <p>
                       Please see the attached updated <b>Safety Statistics Report (SSR)</b>
@@ -91,7 +91,7 @@ html_content = f"""<div style="background:0 0;margin:0;padding:0;border:0 none t
                     <p>
                       Also, kindly review the brief summary in the table below:
                     </p>
-                    <table style="border-collapse:collapse;width:100%;font-family:Helvetica,system-ui,sans-serif;font-size:0.96em;color:#0061ba!important;">
+                    <table style="border-collapse:collapse;width:100%;font-family:Helvetica,system-ui,sans-serif;font-size:0.96em;color:#0061ba!important;box-sizing:border-box">
                         {table_rows}
                     </table>
                     <p>
@@ -103,14 +103,14 @@ html_content = f"""<div style="background:0 0;margin:0;padding:0;border:0 none t
               </tr>
               <tr>
                 <td>
-                  <div style="border:0 none transparent;border-top:.032em dashed rgba(0,97,186,.69);width:90%;margin:clamp(1.125em,3.882vw + .397em,1.95em) auto"></div>
+                  <div style="border:0 none transparent;border-top:.032em dashed rgba(0,97,186,.69);width:90%;margin:clamp(1.125em,3.882vw + .397em,1.95em) auto;box-sizing:border-box"></div>
                   <p>Best regards,</p>
                 </td>
               </tr>
               <tr>
                 <td align="center">
                   <a href="https://www.hcc.com.ph/" style="display:block;text-decoration:none" target="_blank">
-                    <img src="https://raw.githubusercontent.com/MasterZeeno/Repository/main/zee-signature.png" alt="Jay Ar Adlaon Cimacio, RN, OHN" width="100%" height="auto" style="display:block;max-width:100%;height:auto;border:none">
+                    <img src="https://raw.githubusercontent.com/MasterZeeno/Repository/main/zee-signature.png" alt="Jay Ar Adlaon Cimacio, RN, OHN" width="100%" height="auto" style="display:block;max-width:100%;height:auto;border:none;box-sizing:border-box">
                   </a>
                 </td>
               </tr>
