7efc2aa0aa073550fdf1b961ec846e14f03acb58
MasterZeeno <masterzeeno@outlook.com>
2025-08-07 15:25:31 +0800

    Yawa

--- SSR_HTML_EMAIL/html_builder.py
+++ SSR_HTML_EMAIL/html_builder.py
@@ -167,7 +167,7 @@ HTML_BODY = minify(f"""
                     {HEADERS_HTML}
                 </div>
                 {br(2)}
-                <div {VALIGN};text-align:center;padding:{FONT_SIZE*0.5}px auto;">
+                <div aria-label="Download file" aria-role="button" {VALIGN};text-align:center;padding:{FONT_SIZE*0.5}px auto;">
                     <a href="{WB_DOWNLOAD_LINK}" {VALIGN};width:max-content;display:block;text-decoration:none;margin:auto;border-radius:25px;padding:{FONT_SIZE*0.5}px {FONT_SIZE}px;{bold()};font-size:{FONT_SIZE * 0.875};text-align:center;color:#fff;background-color:{FG_LITE};">
                         Download file
                     </a>
